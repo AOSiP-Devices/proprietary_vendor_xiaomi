52b6d9855b87af4b9e7c8d264347dd6f23a92cbb
Subhajeet Muhuri <kenny3fcb@gmail.com>
2020-01-06 22:37:40 +0530

    xiaomi: Update
    Nuke jasmine_sprout
    Update sdm660-common, whyred from ten-sdm660 branch
    Add wayne, wayne-common from ten-sdm660 branch

--- whyred/BoardConfigVendor.mk
+++ whyred/BoardConfigVendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 AOSiP
+# Copyright (C) 2019-20 LineageOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
