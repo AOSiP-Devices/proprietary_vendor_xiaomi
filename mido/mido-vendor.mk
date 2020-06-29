cbb246b73f48575f9c9213f582d13c26c5c74806
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-06-29 17:28:22 +0530

    mido: Remove libbtconfigstore

--- mido/mido-vendor.mk
+++ mido/mido-vendor.mk
@@ -1157,7 +1157,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mido/proprietary/vendor/radio/qcril_database/qcril.db:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/qcril.db
 
 PRODUCT_PACKAGES += \
-    libbtconfigstore \
     libgpustats \
     libqmiservices \
     libsdm-disp-vndapis \
